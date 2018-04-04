503ea58b64acd7c9f1bf5b17f75cf186bcecf148
Lisa Li <wanlinlirosaline@gmail.com>
2018-04-04 16:36:06 -0400

    Update sensorCode.ino

--- sensorCode/sensorCode.ino
+++ sensorCode/sensorCode.ino
@@ -1,20 +1,25 @@
+//A0 ACC A1 POT
 #define SENSORPINA A0 // x axis
- //TODO: define other sensor inputs
+#define SENSORPINB A1 // y axis
 
+const int Pins = 2;
+int button
 unsigned long targetTime=0;
-const unsigned long interval=2500; //TODO: How fast should we read
+const unsigned long interval=2500; 
 void setup(){
 // TODO: begin the serial connection with a baudrate of 115200
+Serial.begin(115200);
+	pinMode()
 }
-
-
 void loop(){
 	if(millis()>=targetTime){
 		targetTime= millis()+interval;
 		Serial.println(analogRead(SENSORPINA));
 
-		 //TODO: Add other sensor read outs
+//TODO: Add other sensor read outs
+		
      //TODO: convert values into a string https://www.arduino.cc/en/Tutorial/StringConstructors
+	
 		 //TODO: combine them into a string that can be understood by server.js
 		 //TODO: send the string over serial
 
