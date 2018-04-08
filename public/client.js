2c905932c5b209aff493849d18fe8d7f4edca249
Lisa Li <wanlinlirosaline@gmail.com>
2018-04-08 16:00:53 -0400

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,38 +1,41 @@
-var COLOUR =  '#505050';  // This is the drawing color
-var radius = 3;           // Constant radio for the line
+var COLOUR =  '#ef97ae';  // pink
+var radius = 5;           // Constant radio for the line
 var socket = io();        // websocket to the server
-var previousPosition=[0,0]; // previous position to draw a line from
+var previousPosition=[0,0]; // previous position
 var ctx = Sketch.create(); //Creating the drawing context
-var firstMessage=true;    // What the first message, to start on the first value
+var firstMessage=true;    // first value
 
     ctx.container = document.getElementById( 'container' ); //reference drawing canvas
     ctx.autoclear= false; // making sure it stays
     ctx.retina='auto';
     ctx.setup = function() { console.log( 'setup' );} // Setup all variables
     ctx.keydown= function() { if ( ctx.keys.C ) ctx.clear();} // handeling keydowns
-
-    socket.on('reset', function() { // on a 'reset' message clean and reste firstMessage
+// on a 'reset' message clean and reste firstMessage
+    socket.on('reset', function() { 
       firstMessage=true;
       ctx.clear();
     });
-
-    socket.on('new-pos', function(newPosition) { // handling new sensor values
-
-      //TODO: Map the incoming 10-bit numbers to the height and width of the screen.
-      // See https://github.com/soulwire/sketch.js/wiki/API for sketch references
+// handling new sensor values
+    socket.on('new-pos', function(cord) { 
+//Map the incoming 10-bit numbers
+      console.log(cord);
+      var x = 1000 -(cord[0] - 250) * 1000 / 150.0
+      var y = (cord[1] - 250)*1000/250.0;
+      var r = Math.max((1000 - cord[2]) / 6.0, radius);
+      var p = [x, y];
 
       if(firstMessage){ // if its the first message store that value as previous
         firstMessage=false;
-        previousPosition=newPosition;
+        previousPosition=p;
       }else{ // any other message we use to draw.
         ctx.lineCap = 'round';
         ctx.lineJoin = 'round';
         ctx.fillStyle = ctx.strokeStyle = COLOUR;
-        ctx.lineWidth = radius;
+        ctx.lineWidth = r;
         ctx.beginPath();  //begin a adrawing
         ctx.moveTo( previousPosition[0], previousPosition[1] ); // from
         ctx.lineTo( newPosition[0],  newPosition[1]); // to
         ctx.stroke(); // and only draw a stroke
-        previousPosition=newPosition; // update to the new position.
+        previousPosition=p; // update to the new position.
        }
     });
